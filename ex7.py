53b36dad9c26f8b05ddc579338143a7b6d717a95
noamoss <noam@thepitz.io>
2020-03-14 22:46:34 +0200

    exerciese 7

---
+++ ex7.py
@@ -0,0 +1,22 @@
+def isPrime(num):
+    i = 1
+    while i<num:
+        if num % i == 0 and i != 1:
+            return False
+        i=i+1
+    return True
+
+def skip_devided(x, sety):
+    for y in sety:
+        if x % y == 0 and x!=1 and y!=1:
+            return True
+    return False
+
+def find_prime_by_location(x):
+    i = 1
+    results = set()
+    while len(results) < x:
+        i+=1
+        if not(skip_devided(i, results)) and isPrime(i):
+            results.add(i)
+    return max(results)
